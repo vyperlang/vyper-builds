5e1683844d5f32f6eac96cc8330ef435536ea1b0
Charles Cooper <cooper.charles.m@gmail.com>
2023-05-18 17:58:40 -0400

    improve directory structure - sort by time of day

--- fetch.py
+++ fetch.py
@@ -47,9 +47,11 @@ if __name__ == "__main__":
         commit_hash = commit_info["id"]
         short_commit_hash = commit_hash[12:]
 
-        # extract into ./builds/2021/03/30/6307049f071a8f5857777c87bb5d858d28112acf/
+        # sorted by time of day.
+        commit_id = commit_time.strftime("%H:%M:%SZ-") + short_commit_hash
+        # extract into ./builds/2023/03/30/12:30:05Z-6307049f071a8f5857777c87bb5d858d28112acf/
         date_part = commit_time.strftime("%Y/%m/%d")
-        directory_part = f"{date_part}/{short_commit_hash}/"
+        directory_part = f"{date_part}/{commit_id}/"
         output_directory = f"builds/{directory_part}/"
         tmp_directory = f"tmp/{date_part}/"
 
